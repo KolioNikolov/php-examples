7dd4bdbbaccf13f551571e503768893ad493f7f9
KolioNikolov <64667958+KolioNikolov@users.noreply.github.com>
2020-05-03 19:10:51 +0300

    Small edit + testing it works

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+echo "WE LIKE SMALL TITTIES";
 
 //require __DIR__ . "/examples/1-function-call-return.php";
 //require __DIR__ . "/examples/2-function-call-parameters.php";
 //require __DIR__ . "/examples/3-function-call-parameters.php";
+
 
